75bbd82b2787a8de5b15ec5133a0f6c7bdb76782
FelipePeres <felippegperes@gmail.com>
2021-04-28 12:22:38 -0300

    first commit

---
+++ src/app/pages/entries/shared/entry.service.ts
@@ -0,0 +1,91 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+import { Observable, throwError } from "rxjs";
+import { map, catchError, flatMap } from "rxjs/operators";
+import { CategoryService } from "./../../categories/shared/category.service";
+
+import { Entry } from "./entry.model";
+
+@Injectable({
+  providedIn: "root",
+})
+export class EntryService {
+  private apiPath: string = "api/entries";
+
+  constructor(
+    private http: HttpClient,
+    private categoryService: CategoryService
+  ) {}
+
+  getAll(): Observable<Entry[]> {
+    return this.http
+      .get(this.apiPath)
+      .pipe(catchError(this.handleError), map(this.jsonDataEntries));
+  }
+
+  getById(id: number): Observable<Entry> {
+    const url = `${this.apiPath}/${id}`;
+    return this.http
+      .get(url)
+      .pipe(catchError(this.handleError), map(this.jsonDataToEntry));
+  }
+
+  create(entry: Entry): any {
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap((category) => {
+        entry.category = category;
+        return this.http
+          .post(this.apiPath, entry)
+          .pipe(catchError(this.handleError), map(this.jsonDataToEntry));
+      })
+    );
+  }
+
+  update(entry: Entry): any {
+    const url = `${this.apiPath}/${entry.id}`;
+
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap((category) => {
+        entry.category = category;
+        return this.http.put(url, entry).pipe(
+          catchError(this.handleError),
+          map(() => entry)
+        );
+      })
+    );
+  }
+
+  delete(id: any): Observable<any> {
+    const url = `${this.apiPath}/${id}`;
+    return this.http.delete(url).pipe(
+      catchError(this.handleError),
+      map(() => null)
+    );
+  }
+
+  /**
+   *
+   * METHODS PRIVATES
+   */
+
+  private jsonDataToEntry(jsonData: any): Entry {
+    return Object.assign(new Entry(), jsonData);
+  }
+
+  private jsonDataEntries(jsonData: any[]): Entry[] {
+    /*console.log(jsonData[0] as Entry);
+    console.log(Object.assign(new Entry(), jsonData[0]));*/
+    const entries: Entry[] = [];
+    jsonData.forEach((element) => {
+      const entry = Object.assign(new Entry(), element);
+      entries.push(entry);
+    });
+    return entries;
+  }
+
+  private handleError(error: any): Observable<any> {
+    console.log("ERRO NA REQUISIÇÃO =>", error);
+    return throwError(error);
+  }
+}
